d4d3f2a95e8044461cdafbe296fc848e49bc3dde
bariyard <worawitrr@gmail.com>
2023-02-10 17:01:20 +0700

    [Preview] hand dataset

--- pages/index.tsx
+++ pages/index.tsx
@@ -7,6 +7,7 @@ import WvNavbar from '@wevisdemo/ui/react/navbar';
 import WvNavButton from '@wevisdemo/ui/react/nav-button';
 import WvFooter from '@wevisdemo/ui/react/footer';
 import { useRouter, usePathname } from 'next/navigation'
+import TestNoco from '../components/TestNoco';
 
 export default function Home() {
   const router = useRouter();
@@ -17,10 +18,14 @@ export default function Home() {
         <WvNavButton active={pathname === '/'} onClick={() => router.replace('/')}>Home</WvNavButton>
         <WvNavButton active={pathname === '/about'} onClick={() => router.replace('/about')}>About</WvNavButton>
       </WvNavbar>
+      <TestNoco />
       <WvContainer>
         <h1 className="text-3xl font-bold underline">
           Hello, Next.js!
         </h1>
+        <div className='ประยุทธ์-จันโอชา'>
+          ปปปปปปป
+        </div>
         <TestBTN />
         <TestStyle />
       </WvContainer>
